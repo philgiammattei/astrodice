b422062a3b753e86164307a8239a21778dcaacc4
Phil Giammattei <philgiammattei@gmail.com>
2022-08-05 14:37:02 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const planets = ['sun', 'moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto', 'north node', 'south node'];
+  const zodiac = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'];
+  const houses = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighths', 'ninth', 'tenth', 'eleventh', 'twelfth'];
+
+  function pickOneFromTwelve(group) {
+    const index = Math.floor(Math.random() * 12)
+    console.log(index);
+    return group[index];
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>astrodice</h2>
+        <div className="dieValue">
+          {pickOneFromTwelve(planets)}
+        </div>
+        <div className="dieValue">
+          {pickOneFromTwelve(zodiac)}
+        </div>
+        <div className="dieValue">
+          {pickOneFromTwelve(houses)} house
+        </div>
       </header>
+
+
     </div>
   );
 }
