84afc8d678d0efbdcfd386a721086a2ba545f78e
Phil Giammattei <philgiammattei@gmail.com>
2022-08-07 12:16:30 -0400

    toggle between symbol and full name

--- src/App.js
+++ src/App.js
@@ -5,10 +5,54 @@ function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [numbers, setNumbers] = useState([]);
+  const [isShowingSymbols, setIsShowingSymbols] = useState(true);
 
-  const planets = ['sun', 'moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto', 'north node', 'south node'];
-  const zodiac = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'];
-  const houses = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
+  const planets = [
+    { name: 'sun', symbol: '☉' },
+    { name: 'moon', symbol: '☽︎' },
+    { name: 'mercury', symbol: '☿' },
+    { name: 'venus', symbol: '♀' },
+    { name: 'mars', symbol: '♂' },
+    { name: 'jupiter', symbol: '♃' },
+    { name: 'saturn', symbol: '♄' },
+    { name: 'uranus', symbol: '♅' },
+    { name: 'neptune', symbol: '♆' },
+    { name: 'pluto', symbol: '♇' },
+    { name: 'north node', symbol: '☊' },
+    { name: 'south node', symbol: '☋' },
+  ];
+
+  const zodiac = [
+    { name: 'aries', symbol: '♈︎' },
+    { name: 'taurus', symbol: '♉︎' },
+    { name: 'gemini', symbol: '♊︎' },
+    { name: 'cancer', symbol: '♋︎' },
+    { name: 'leo', symbol: '♌︎' },
+    { name: 'virgo', symbol: '♍︎' },
+    { name: 'libra', symbol: '♎︎' },
+    { name: 'scorpio', symbol: '♏︎' },
+    { name: 'sagittarius', symbol: '♐︎' },
+    { name: 'capricorn', symbol: '♑︎' },
+    { name: 'aquarius', symbol: '♒︎' },
+    { name: 'pisces', symbol: '♓︎' },
+  ];
+
+  const houses = [
+    { name: 'first', symbol: '1' },
+    { name: 'second', symbol: '2' },
+    { name: 'third', symbol: '3' },
+    { name: 'fourth', symbol: '4' },
+    { name: 'fifth', symbol: '5' },
+    { name: 'sixth', symbol: '6' },
+    { name: 'seventh', symbol: '7' },
+    { name: 'eighth', symbol: '8' },
+    { name: 'ninth', symbol: '9' },
+    { name: 'tenth', symbol: '10' },
+    { name: 'eleventh', symbol: '11' },
+    { name: 'twelfth', symbol: '12' },
+  ];
+
+
 
   useEffect(() => {
     fetch('https://qrng.anu.edu.au/API/jsonI.php?length=3&type=uint8')
@@ -34,14 +78,17 @@ function App() {
         {!error && !isLoaded && <div>Loading...</div>}
         {!error && isLoaded &&
           <>
-            <div className="dieValue">
-              {planets[numbers[0]]}
+            <div className="die-value">
+              {isShowingSymbols ? <div className="symbol">{planets[numbers[0]].symbol}</div> : planets[numbers[0]].name}
             </div>
-            <div className="dieValue">
-              {zodiac[numbers[1]]}
+            <div className="die-value">
+              {isShowingSymbols ? <div className="symbol">{zodiac[numbers[1]].symbol}</div> : zodiac[numbers[1]].name}
             </div>
-            <div className="dieValue">
-              {houses[numbers[2]]} house
+            <div className="die-value">
+              {isShowingSymbols ? houses[numbers[2]].symbol : houses[numbers[2]].name + ' house'}
+            </div>
+            <div className="symbol-name-toggle">
+              <a href="#" onClick={() => setIsShowingSymbols(!isShowingSymbols)}>{isShowingSymbols ? 'show full name' : 'show symbols'}</a>
             </div>
           </>}
       </header>
